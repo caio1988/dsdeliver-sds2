5dc5e1f31586b3c26a91e6208a80c9918eaa7a26
unknown <caio.orlandini@hotmail.com>
2021-01-10 11:43:54 -0300

    Add routes and StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
@@ -14,7 +15,7 @@ function Navbar() {
                         <h3 className="home-subtitle">
                             Escolha seu pedido, e em poucos minutos <br /> levaremos na sua porta.
                     </h3>
-                        <a href="orders" className="home-btn-order"> FAZER PEDIDO </a>
+                        <Link to="/orders" className="home-btn-order"> FAZER PEDIDO </Link>
                     </div>
                     <div className="home-image"> <MainImage /> </div>
                 </div>
